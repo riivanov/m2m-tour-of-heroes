ee4e74d973ca1086eb21d1acb56d0a0fea34e600
Radoslav Ivanov <riivanov63@gmail.com>
2024-01-25 16:19:46 +0200

    reset form; save entire hero object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -38,7 +38,7 @@ export class HeroesComponent implements OnInit {
     if (!name) {
       return;
     }
-    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+    this.heroService.addHero(hero).subscribe((hero) => {
       this.heroes.push(hero);
     });
   }
